d1389628c45574c02dfd8d3adc69c67ea74a29b9
watila <watila.costa@fortaleza.ce.gov.br>
2019-05-17 16:57:52 -0300

    Modificacoes contact belongs_to kind

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,16 @@
 class Contact < ApplicationRecord
   belongs_to :kind
 
-  # Sobreescrevendo o método
-  def as_json(options={})
-    super(
-      # root: true,
-      include: {kind: {only: :description } }
-    )
-  end
+  # def kind_description
+  #   kind.description
+  # end
+  #
+  # # Sobreescrevendo o método
+  # def as_json(options={})
+  #   super(
+  #     # root: true,
+  #     methods: [:kind_description],
+  #     include: {kind: {only: :description } }
+  #   )
+  # end
 end
